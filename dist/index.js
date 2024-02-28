5957e3ec8630c63085c24df63dd2b453f1c00b19
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-02-29 00:32:34 +0530

    added basic ts codes in index.ts

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,64 @@ function sumOfAge(user1, user2) {
 }
 const res = sumOfAge({ name: "Pratik", age: 25 }, { name: "test", age: 29 });
 console.log(res);
+function Greet(firstName) {
+    const greet = "Good Morning" + firstName;
+    console.log(greet);
+}
+Greet("Pratik");
+//type inference
+function Sum(a, b) {
+    return a + b;
+}
+const value = Sum(2, 3);
+console.log(value);
+function isLegal(age) {
+    if (age > 18) {
+        return true;
+    }
+    return false;
+}
+const age = isLegal(23);
+console.log(age);
+function runaftersec(fn) {
+    setTimeout(fn, 1000);
+}
+runaftersec(function () {
+    console.log("hi there");
+});
+;
+function isLegalUser(user) {
+    if (user.age > 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+isLegalUser({
+    age: 20
+});
+var Direction;
+(function (Direction) {
+    Direction[Direction["up"] = 0] = "up";
+    Direction[Direction["down"] = 1] = "down";
+    Direction[Direction["left"] = 2] = "left";
+    Direction[Direction["right"] = 3] = "right";
+})(Direction || (Direction = {}));
+function doSomething(keypress) {
+    console.log(Direction.left);
+}
+doSomething(Direction.up);
+function identity(arg) {
+    return arg;
+}
+let output1 = identity("mystring");
+let output2 = identity(100);
+output1.toUpperCase();
+function getFirstElement(arr) {
+    return arr[0];
+}
+const el = getFirstElement(["pratik", "prajapati"]);
+const el2 = getFirstElement([{ fname: "pratikpp" }]);
+console.log(el2.fname);
+console.log(el);
--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,88 @@ console.log(res);
 
 
 
+function Greet(firstName : String){
+    const greet = "Good Morning"+ firstName;
+    console.log(greet);
+    
+}
+
+Greet("Pratik");
+
+//type inference
+function Sum(a : number,b : number):number{
+    return a + b;
+}
+
+const value = Sum(2,3);
+console.log(value);
+
+function isLegal(age : number) : boolean{
+    if(age > 18){
+        return true;
+    }
+    return false;
+}
+
+const age = isLegal(23);
+console.log(age);
+
+function runaftersec(fn : ()=> void){
+    setTimeout(fn,1000);
+}
+runaftersec(function(){
+    console.log("hi there");
+    
+})
+
+interface UserV{
+    name? : String,
+    age: number
+};
+function isLegalUser(user : UserV){
+    if(user.age > 18){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+isLegalUser({
+    age :20
+})
+
+
+enum Direction{
+    up,
+    down,
+    left,
+    right
+}
+
+function doSomething(keypress : Direction){
+    console.log(Direction.left);
+}
+
+doSomething(Direction.up);
+
+
+function identity<T>(arg : T):T{
+    return arg;
+}
+let output1 = identity<string>("mystring");
+let output2 = identity<number>(100);
+
+output1.toUpperCase();
+
+
+function getFirstElement<T>(arr : T[]):T{
+    return arr[0];
+}
+
+interface Userss{
+    fname : string;
+}
+const el = getFirstElement<string>(["pratik","prajapati"])
+const el2 = getFirstElement<Userss>([{fname : "pratikpp"}])
+console.log(el2.fname);
+console.log(el);
